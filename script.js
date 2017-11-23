f04cdf2a0474233702f3780f4076883b1c1bad90
Stephanie Lam <m.stephanielam@gmail.com>
2017-11-22 18:03:00 -0800

    Implement pages

--- script.js
+++ script.js
@@ -2,261 +2,611 @@ $(function(){
   console.log("jquery loaded");
   let groups = [
     {
-      name: "1",
-      short: "1",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$0",
+      name: "VanHikers",
+      short: "VH",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 79,
+      created: "Jan 2016",
+      cost: 25,
       next_event: "",
       tags: ["Fitness", "Bicycling"],
       organizers: "",
       status: ""
     },
     {
-      name: "2",
-      short: "2",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Food Lovers & Bloggers",
+      short: "FL",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Burnaby",
+      members: 22,
+      created: "Jan 2016",
+      cost: 0,
       next_event: "",
       tags: ["Social", "Adventure", "Hiking"],
       organizers: "",
       status: ""
     },
     {
-      name: "3",
-      short: "3",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Pick-Up Sports",
+      short: "PS",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 120,
+      created: "Nov 2017",
+      cost: 0,
+      next_event: "",
+      tags: ["Fitness", "Hiking"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "VanJS",
+      short: "VJ",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Victoria",
+      members: 20,
+      created: "August 2017",
+      cost: 40,
       next_event: "",
       tags: ["Social", "Adventure"],
       organizers: "",
       status: ""
     },
     {
-      name: "4",
-      short: "4",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Urban Minglers 30+",
+      short: "UM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 32,
+      created: "May 2017",
+      cost: 0,
       next_event: "",
       tags: ["Fitness", "Social"],
       organizers: "",
       status: ""
     },
     {
-      name: "5",
-      short: "5",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Tech Vancouver",
+      short: "TV",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 58,
+      created: "April 2016",
+      cost: 10,
       next_event: "",
       tags: ["Adventure"],
       organizers: "",
       status: ""
     },
     {
-      name: "6",
-      short: "6",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Code and Coffee",
+      short: "CC",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Richmond",
+      members: 42,
+      created: "Jan 2016",
+      cost: 0,
       next_event: "",
       tags: ["Fitness", "Bicycling", "Social", "Adventure", "Hiking"],
       organizers: "",
       status: ""
     },
     {
-      name: "7",
-      short: "7",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Vancouver Brainiacs",
+      short: "VB",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 16,
+      created: "Feb 2017",
+      cost: 30,
       next_event: "",
       tags: ["Fitness", "Bicycling", "Social", "Adventure", "Hiking"],
       organizers: "",
       status: ""
     },
     {
-      name: "8",
-      short: "8",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "Single Professionals",
+      short: "SP",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 210,
+      created: "Dec 2015",
+      cost: 25,
       next_event: "",
       tags: ["Fitness", "Hiking"],
       organizers: "",
       status: ""
     },
     {
-      name: "9",
-      short: "9",
-      location: "Vancouver, BC",
-      members: 50,
-      founded: "Jan 2016",
-      cost: "$10",
+      name: "English Conversation Club",
+      short: "EC",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Richmond",
+      members: 102,
+      created: "Nov 2017",
+      cost: 0,
       next_event: "",
       tags: ["Fitness", "Hiking"],
       organizers: "",
       status: ""
     },
+    {
+      name: "Cooking Mamas",
+      short: "CM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Richmond",
+      members: 120,
+      created: "Nov 2017",
+      cost: 0,
+      next_event: "",
+      tags: ["Fitness", "Hiking"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Outdoor Adventure",
+      short: "OA",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 40,
+      created: "Nov 2014",
+      cost: 0,
+      next_event: "",
+      tags: ["Adventure", "Outdoors", "Hiking"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Rowing Club",
+      short: "RC",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 90,
+      created: "Oct 2016",
+      cost: 20,
+      next_event: "",
+      tags: ["Fitness", "Rowing", "Sports"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Vancouver Startups",
+      short: "VN",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 210,
+      created: "Jan 2017",
+      cost: 5,
+      next_event: "",
+      tags: ["Business", "Startups", "Tech"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Movie and Music Fans",
+      short: "MM",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 120,
+      created: "Mar 2013",
+      cost: 20,
+      next_event: "",
+      tags: ["Movies", "Music", "Entertainment"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Sketch Club",
+      short: "SC",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 40,
+      created: "Jul 2015",
+      cost: 0,
+      next_event: "",
+      tags: ["Art", "Drawing"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "Salsa Dance Meetup",
+      short: "SD",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 80,
+      created: "Apr 2014",
+      cost: 15,
+      next_event: "",
+      tags: ["Dance", "Music"],
+      organizers: "",
+      status: ""
+    },
+    {
+      name: "VanFoodies",
+      short: "VF",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      location: "Vancouver",
+      members: 100,
+      created: "Mar 2015",
+      cost: 30,
+      next_event: "",
+      tags: ["Restaurants", "Food"],
+      organizers: "",
+      status: ""
+    }
   ]
 
   var format_group = function(group) {
     html = "<h2>"+group['name']+"</h2>"
-    if (filters.indexOf("location") > -1){
+    if (attributes.indexOf("description") > -1){
+      html += "<p>"+group['description']+"</p>"
+    }
+
+    if (attributes.indexOf("location") > -1){
       html += "<p>"+group['location']+"</p>"
     }
 
-    if (filters.indexOf("members") > -1){
+    if (attributes.indexOf("members") > -1){
       html += "<p>Members: "+group['members']+"</p>"
     }
 
-    if (filters.indexOf("cost") > -1){
-      html += "<p>Cost: "+group['cost']+"</p>"
+    if (attributes.indexOf("cost") > -1){
+      html += "<p>Cost: $"+group['cost']+"</p>"
     }
 
-    if (filters.indexOf("next_event") > -1){
+    if (attributes.indexOf("next_event") > -1){
       html += "<p>Next Event: "+group['next_event']+"</p>"
     }
 
-    if (filters.indexOf("founded") > -1){
-      html += "<p>Next Event: "+group['next_event']+"</p>"
+    if (attributes.indexOf("created") > -1){
+      html += "<p>Created: "+group['created']+"</p>"
     }
 
-    if (filters.indexOf("tags") > -1){
+    if (attributes.indexOf("tags") > -1){
       html += "<p>Tags: "+group['tags']+"</p>"
     }
 
-    if (filters.indexOf("organizers") > -1){
+    if (attributes.indexOf("organizers") > -1){
       html += "<p>Organizers: "+group['organizers']+"</p>"
     }
     return html
   }
 
-  var set_group = function(id) {
-    while (groups[next]['status'] == 'favourite' ||
-      groups[next]['status'] == 'delete' ){
-      next++
+  var hide_all = function() {
+    $('#filters').css("display", "none")
+    $('#browse-groups').css("display", "none")
+    $('#bookmarked-groups').css("display", "none")
+    $('#faceoff-groups').css("display", "none")
+    $('#show-group').css("display", "none")
+  }
+
+  var get_groups = function() {
+    valid_groups = []
+
+    for (let i=0; i<groups.length; i++){
+      if (groups[i]['status'] != 'favourite' && groups[i]['status'] != 'delete'){
+        if (match_filter(groups[i])){
+          valid_groups.push(groups[i])
+        }
+      }
     }
-    document.getElementById("group_"+id).innerHTML = format_group(groups[next])
+  }
+
+  var set_groups = function() {
+    if (valid_groups[0] != undefined){
+      document.getElementById("pos_1").innerHTML = format_group(valid_groups[0])
+    } else {
+      document.getElementById("pos_1").innerHTML = ''
+    }
+    if (valid_groups[1] != undefined){
+      document.getElementById("pos_2").innerHTML = format_group(valid_groups[1])
+    } else {
+      document.getElementById("pos_2").innerHTML = ''
+    }
+    if (valid_groups[2] != undefined){
+      document.getElementById("pos_3").innerHTML = format_group(valid_groups[2])
+    } else {
+      document.getElementById("pos_3").innerHTML = ''
+    }
+    if (valid_groups[3] != undefined){
+      document.getElementById("pos_4").innerHTML = format_group(valid_groups[3])
+    } else {
+      document.getElementById("pos_4").innerHTML = ''
+    }
+    pos_1 = 0
+    pos_2 = 1
+    pos_3 = 2
+    pos_4 = 3
+    next  = 4
+  }
+
+  var set_favourites = function() {
+    favourites = []
+    for (let group of groups){
+      if (group['status'] == 'favourite'){
+        favourites.push(group)
+      }
+    }
+
+    html = "<h2>Cart</h2>"
+    for (let i=0; i<favourites.length; i++){
+      html+="<div class='col-md-4'> <div id='fav_" + i + "' class='group fav-group'>"+format_group(favourites[i]) + "</div></div>"
+      document.getElementById("bookmarked-groups").innerHTML = html
+    }
+  }
+
+  var set_faceoff = function() {
+    console.log("FACEOFF")
+    hide_all()
+    $('#faceoff-groups').css("display", "")
+    document.getElementById("fev").innerHTML = format_group(favourites[0])
+    document.getElementById("fev2").innerHTML = format_group(favourites[1])
+    document.getElementById("fev3").innerHTML = format_group(favourites[2])
+  }
+
+  var set_group = function(id) {
+    if (valid_groups[next] != undefined){
+      document.getElementById("pos_"+id).innerHTML = format_group(valid_groups[next])
+    } else {
+      document.getElementById("pos_"+id).innerHTML = ''
+    }
     next++
   }
 
-  var add_cart = function(i) {
-    cart_html += "<li>"+groups[i]['short']+"</li>"
-    document.getElementById("cart-container").innerHTML =
-    "<ul>"+cart_html+"</ul>"
+  var show_group = function(id) {
+    console.log(id)
+    console.log(favourites[id])
+    hide_all()
+    $('#show-group').css("display", "")
+    html = "<h2>"+favourites[id]['name']+"</h2>"
+    html+="<div class='group fav-group'>"+format_group(favourites[id]) + "</div>"
+    document.getElementById("show-group").innerHTML = html
   }
 
-  var refresh_groups = function() {
-    document.getElementById("group_1").innerHTML = format_group(groups[group_1])
-    document.getElementById("group_2").innerHTML = format_group(groups[group_2])
-    document.getElementById("group_3").innerHTML = format_group(groups[group_3])
-    document.getElementById("group_4").innerHTML = format_group(groups[group_4])
-  }
-
-  $('#filter-submit').click(function(e){
-    update_filters()
+  $('#face-0').click(function(id){
+    console.log("SHOW GROUP")
+    // show_group(id)
+  });
+    $('#face-1').click(function(id){
+    console.log("SHOW GROUP")
+    // show_group(id)
+  });
+      $('#face-2').click(function(id){
+    console.log("SHOW GROUP")
+    // show_group(id)
   });
 
-  var update_filters = function(){
-    filters = []
+
+  var add_cart = function(i) {
+    cart_html += "<li>"+valid_groups[i]['short']+"</li>"
+    document.getElementById("cart-items").innerHTML = cart_html
+  }
+
+  var update_attributes = function(){
+    attributes = []
+    if ($('#description').is(":checked")){
+      attributes.push("description")
+    }
     if ($('#location').is(":checked")){
-      filters.push("location")
+      attributes.push("location")
     }
     if ($('#members').is(":checked")){
-      filters.push("members")
+      attributes.push("members")
     }
-    if ($('#founded').is(":checked")){
-      filters.push("founded")
+    if ($('#created').is(":checked")){
+      attributes.push("created")
     }
     if ($('#cost').is(":checked")){
-      filters.push("cost")
+      attributes.push("cost")
     }
     if ($('#next_event').is(":checked")){
-      filters.push("next_event")
+      attributes.push("next_event")
     }
     if ($('#tags').is(":checked")){
-      filters.push("tags")
+      attributes.push("tags")
     }
     if ($('#organizers').is(":checked")){
-      filters.push("organizers")
+      attributes.push("organizers")
     }
-    refresh_groups()
+    set_groups()
+  }
+
+  var match_filter = function(group){
+    filter = {
+      location: $('#location-filter').val(),
+      members: $('#members-filter').val(),
+      cheap: $('#cheap-filter').is(":checked"),
+      moderate: $('#moderate-filter').is(":checked"),
+      expensive: $('#expensive-filter').is(":checked"),
+      filters: []
+    }
+    if (group['location'] != filter['location'] && filter['location'] != '') {
+      return false
+    }
+
+    if (group['members'] < filter['members'] && filter['members'] != '') {
+      return false
+    }
+
+    if (!(group['cost'] == 0) && filter['cheap'] == true) {
+      return false
+    }
+
+    if (!(group['cost'] > 0 & group['cost'] <= 20) && filter['moderate'] == true) {
+      return false
+    }
+
+    if (!(group['cost'] > 20) && filter['expensive'] == true) {
+      return false
+    }
+
+    // if (group['filters'] != filter['filters']) {
+    //   return false
+    // }
+
+    return true
   }
 
   var initialize = function() {
     cart_html = ""
-    group_1 = 0
-    group_2 = 1
-    group_3 = 2
-    group_4 = 3
+    pos_1 = 0
+    pos_2 = 1
+    pos_3 = 2
+    pos_4 = 3
     next = 4
+    valid_groups = groups
+    filter = {}
+    favourites = []
 
-    update_filters()
+    update_attributes()
+    set_groups()
+    hide_all()
+    $('#browse-groups').css("display", "")
+    $('#filters').css("display", "")
   }
 
   initialize()
 
+  $('#attribute-submit').click(function(e){
+    update_attributes()
+  });
+
+  $('#filter-submit').click(function(e){
+    get_groups()
+    set_groups()
+  });
+
+  $('#faceoff-submit').click(function(e){
+    set_favourites()
+    if (favourites.length >=3){
+      $('#faceoff-groups').css("display", "")
+      $('#bookmarked-groups').css("display", "none")
+      set_faceoff()
+    } else {
+      alert('add at least 3 groups to cart!')
+    }
+  });
+
+  $('#add-groups').click(function(){
+    hide_all()
+    $('#browse-groups').css("display", "")
+    $('#filters').css("display", "")
+  });
+
+    $('#logo').click(function(){
+    hide_all()
+    $('#browse-groups').css("display", "")
+    $('#filters').css("display", "")
+  });
+
+  $('#cart').click(function(){
+    set_favourites()
+
+    hide_all()
+    $('#bookmarked-groups').css("display", "")
+  });
+
   $('#love_1').click(function(){
-    groups[group_1]['status'] = 'favourite'
-    add_cart(group_1)
-    group_1 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_1] == groups[i]){
+        groups[i]['status'] = 'favourite'
+      }
+    }
+    add_cart(pos_1)
+    pos_1 = next
     set_group(1)
   });
 
   $('#love_2').click(function(){
-    groups[group_2]['status'] = 'favourite'
-    add_cart(group_2)
-    group_2 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_2] == groups[i]){
+        groups[i]['status'] = 'favourite'
+      }
+    }
+    add_cart(pos_2)
+    pos_2 = next
     set_group(2)
   });
 
   $('#love_3').click(function(){
-    groups[group_3]['status'] = 'favourite'
-    add_cart(group_3)
-    group_3 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_3] == groups[i]){
+        groups[i]['status'] = 'favourite'
+      }
+    }
+    add_cart(pos_3)
+    pos_3 = next
     set_group(3)
   });
 
   $('#love_4').click(function(){
-    groups[group_4]['status'] = 'delete'
-    group_4 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_4] == groups[i]){
+        groups[i]['status'] = 'favourite'
+      }
+    }
+    add_cart(pos_4)
+    pos_4 = next
     set_group(4)
   });
 
   $('#delete_1').click(function(){
-    groups[group_1]['status'] = 'delete'
-    group_1 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_1] == groups[i]){
+        groups[i]['status'] = 'delete'
+      }
+    }
     set_group(1)
+    pos_1 = next
   });
 
   $('#delete_2').click(function(){
-    groups[group_2]['status'] = 'delete'
-    group_2 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_1] == groups[i]){
+        groups[i]['status'] = 'delete'
+      }
+    }
     set_group(2)
+    pos_2 = next
   });
 
   $('#delete_3').click(function(){
-    groups[group_3]['status'] = 'delete'
-    group_3 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_1] == groups[i]){
+        groups[i]['status'] = 'delete'
+      }
+    }
     set_group(3)
+    pos_3 = next
   });
 
   $('#delete_4').click(function(){
-    groups[group_4]['status'] = 'delete'
-    group_4 = next
+    for (var i=0; i<groups.length; i++) {
+      if (valid_groups[pos_1] == groups[i]){
+        groups[i]['status'] = 'delete'
+      }
+    }
     set_group(4)
+    pos_4 = next
+  });
+
+
+  $('#choose_1').click(function(){
+    console.log("CHOOSE 1")
+    show_group(0)
+  });
+
+  $('#choose_2').click(function(){
+    console.log("CHOOSE 2")
+
+    show_group(1)
+  });
+
+  $('#choose_3').click(function(){
+    console.log("CHOOSE 3")
+
+    show_group(2)
   });
 
 });
